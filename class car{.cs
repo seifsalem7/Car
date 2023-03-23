9577aae6f7da3b0936fc0728c08bbcc8af374bad
seifsalem7 <128747690+seifsalem7@users.noreply.github.com>
2023-03-23 22:43:30 +0200

    Add files via upload

---
+++ class car{.cs
@@ -0,0 +1,30 @@
+class car{
+    public string model; 
+    public int speed; 
+    public string color; 
+    public int year; 
+    public int number_of_passengers; 
+    public int price; 
+    public int horse_power;
+    public int number_of_wheel; 
+
+    public void car ()
+    {model = "BMW";
+    speed = 280;
+    color = "blue";
+    year = 2022;
+    number_of_passengers = 4;
+    price = 2000000;
+    horse_power = 700;
+    number_of_wheel = 4;
+    }
+
+
+    void turnon(){}
+    void turnoff(){}
+    void move(){}
+    void changespeed(){}
+    void lightup(){}
+    void beep(){}
+    
+}
